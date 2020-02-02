8932fdef693ca94d4209515ebda45b442c011aa6
Dimitris Tsiktsiris <ditsikts@outlook.com>
2020-02-02 08:02:02 +0200

    common index.html

--- main.js
+++ main.js
@@ -4,6 +4,14 @@ var path = require('path');
 
 app.use(express.static('static'));
 
+app.get('/editor', function(req, res) {
+    res.sendFile(path.join(__dirname + '/static/edit.html'));
+});
+
+app.get('/elements', function(req, res) {
+    res.sendFile(path.join(__dirname + '/static/elem.html'));
+});
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/static/index.html'));
 });
