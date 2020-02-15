76396a37f8d9d7f90568f94b9c41ea9abc5c0083
Dimitris Tsiktsiris <ditsikts@outlook.com>
2020-02-15 20:33:38 +0200

    add single left param recogn

--- static/edit.js
+++ static/edit.js
@@ -39,8 +39,19 @@ const wordAtIndex = (plain, caretPos) => {
   return [word, forWord, backWord - 1];
 }
 
-let previousPlain = '';
-// let plain = '';
+function isWord(str) {
+  var code, i, len;
+
+  for (i = 0, len = str.length; i < len; i++) {
+    code = str.charCodeAt(i);
+    if (/*!(code > 47 && code < 58) && // numeric (0-9) */
+      !(code > 64 && code < 91) && // upper alpha (A-Z)
+      !(code > 96 && code < 123)) { // lower alpha (a-z)
+      return false;
+    }
+  }
+  return true;
+};
 
 function inChange(e) {
 
@@ -63,15 +74,23 @@ function inChange(e) {
   lis.innerHTML = '';
   const plain = input.innerText;
   // console.log(plain);
-  
+
   const plainArray = input.innerText.split(/(\s+|\w+|\(|\))/).filter(item => item.length > 0);
+  console.log(plainArray);
 
 
   depthIndex = 0;
   opening = true;
 
   let parMarks3 = [];
-  let simiChars = '';
+
+  const checkPreviousTypeWithSpace = (index, type) => {
+    return parMarks3[index - 2].type === type
+      && parMarks3[index - 1].type === 'whitespace';
+  }
+
+
+
   for (let i = 0; i < plainArray.length; i++) {
     if (plainArray[i].charCodeAt(0) === 160 || plainArray[i].charCodeAt(0) === 32) {
       parMarks3.push({ text: plainArray[i].replace(/ /g, '&nbsp;'), cssClass: 'nostyle', type: 'whitespace' })
@@ -94,8 +113,25 @@ function inChange(e) {
       }
       parMarks3.push({ text: plainArray[i], cssClass: 'par' + depthIndex, depth: depthIndex, type: 'closingParentheses' })
     }
-    else if (plainArray[i].toUpperCase === 'AND' || plainArray[i].toUpperCase === 'OR') {
-      parMarks3.push({ text: plainArray[i].toUpperCase, cssClass: 'oper', type: 'operator' })
+    else if (plainArray[i].toUpperCase() === 'AND' || plainArray[i].toUpperCase() === 'OR') {
+      parMarks3.push({ text: plainArray[i].toUpperCase(), cssClass: 'oper', type: 'operator' })
+    }
+    else if (isWord(plainArray[i])) {
+      if (checkPreviousTypeWithSpace(i, 'openingParentheses')) {
+        let found = leftParam[0].props.find(prop => prop.name === plainArray[i]);
+        if (found) {
+          parMarks3.push({ text: found.name, type: leftParam[0].type, cssClass: cssClass[found.country] });
+        }
+        else {
+
+          parMarks3.push({ text: plainArray[i], cssClass: 'error', type: 'error' });
+        }
+      }
+      else {
+
+        parMarks3.push({ text: plainArray[i], cssClass: 'error', type: 'error' });
+      }
+
     }
     else {
       parMarks3.push({ text: plainArray[i], cssClass: 'error', type: 'error' })
@@ -106,89 +142,8 @@ function inChange(e) {
   // return;
 
 
-  // let caretPos = caret.getPos();
-
-  // lis.innerHTML = '';
-
-  // // console.log('start' + plain);
-
-  // depthIndex = 0;
-  // opening = true;
-
-  // let parMarks2 = [];
-  // let simiChars = '';
-
-  // for (let i = 0; i < plain.length; i++) {
-
-  //   if (plain.charCodeAt(i) === 160
-  //     || plain.charCodeAt(i) === 32) {
-  //     simiChars += '&nbsp;';
-  //     i++;
-  //     while (plain.charCodeAt(i) === 160 || plain.charCodeAt(i) === 32) {
-  //       simiChars += '&nbsp;';
-  //       i++;
-  //     }
-  //     i--;
-  //     parMarks2.push({ text: simiChars, cssClass: 'nostyle', type: 'whitespace' })
-  //     simiChars = '';
-  //   }
-  //   else if (plain[i] === '(') {
-  //     if (opening === false) {
-  //       opening = true;
-  //     }
-  //     else {
-  //       depthIndex += 1;
-  //     }
-  //     parMarks2.push({ text: plain[i], cssClass: 'par' + depthIndex, depth: depthIndex, type: 'openingParentheses' })
-  //   }
-  //   else if (plain[i] === ')') {
-  //     if (opening === true) {
-  //       opening = false;
-  //     }
-  //     else {
-  //       depthIndex -= 1;
-  //     }
-  //     parMarks2.push({ text: plain[i], cssClass: 'par' + depthIndex, depth: depthIndex, type: 'closingParentheses' })
-  //   }
-  //   else if (plain[i] === 'O' && plain[i + 1] === 'R'
-  //     && !isLetter(plain[i - 1]) && !isLetter(plain[i + 2])) {
-
-  //     parMarks2.push({ text: 'OR', cssClass: 'oper', type: 'operator' })
-  //     i += 1;
-  //   }
-  //   else if (plain[i] === 'A' && plain[i + 1] === 'N' && plain[i + 2] === 'D'
-  //     && !isLetter(plain[i - 1]) && !isLetter(plain[i + 3])) {
-
-  //     parMarks2.push({ text: 'AND', cssClass: 'oper', type: 'operator' })
-  //     i += 2;
-  //   }
   //   else if (isLetter(plain[i]) && !isLetter(plain[i - 1])) {
 
-  //     if (parMarks2[parMarks2.length - 1].text === '('
-  //       || parMarks2[parMarks2.length - 2].type === 'operator'
-  //       || (parMarks2[parMarks2.length - 2].text === '('
-  //         && parMarks2[parMarks2.length - 1].type === 'whitespace')) {
-
-  //       let notFound = true;
-  //       let iOf = -1;
-  //       let k = 0;
-
-  //       while (notFound && k < leftPart.cities.length) {
-  //         iOf = plain.indexOf(leftPart.cities[k].name, i);
-  //         if (iOf === i) {
-  //           parMarks2.push({ text: leftPart.cities[k].name, type: leftPart.cities[k].type, cssClass: cssClass[leftPart.cities[k].country] });
-  //           notFound = false;
-  //           i += leftPart.cities[k].name.length - 1;
-  //         }
-  //         k += 1;
-  //       }
-
-  //       if (notFound) {
-  //         const [word,] = wordAtIndex(plain, i);
-  //         parMarks2.push({ text: word, cssClass: 'error' });
-  //         i += word.length - 1;
-  //       }
-  //     }
   //     else if ((parMarks2[parMarks2.length - 2].type === 'city'
   //       && parMarks2[parMarks2.length - 1].type === 'whitespace')) {
 
@@ -312,7 +267,7 @@ function inChange(e) {
       || (parMarks3[idx - 2].text === '('
         && parMarks3[idx - 1].type === 'whitespace')
     ) {
-      let ac = leftPart.cities.filter((f) => {
+      let ac = leftParam[0].props.filter((f) => {
         return f.name.includes(word);
       })
       ac.forEach(f => {
@@ -340,7 +295,7 @@ function inChange(e) {
       })
     }
     else if (parMarks3[idx - 2].type === 'buildings'
-      && parMarks2[idx - 1].type === 'whitespace') {
+      && parMarks3[idx - 1].type === 'whitespace') {
 
       groups[0].props.forEach(f => {
         let el = document.createElement('li');
@@ -374,7 +329,7 @@ function inChange(e) {
     formated += '<span class="' + parMarks3[i].cssClass + '">' + parMarks3[i].text + '</span>';
   }
 
-  console.log(parMarks3);
+  // console.log(parMarks3);
   // console.log(formated);
 
 
@@ -422,50 +377,47 @@ const cssClass = {
   states: 'states',
   prop: 'prop'
 }
-const leftPart = {
-  cities: [
-    {
-      "id": "1",
-      "name": "Phoenix",
-      "country": "USA",
-      "type": "city"
-    },
-    {
-      "id": "2",
-      "name": "Florida",
-      "country": "USA",
-      "type": "city"
-    },
-    {
-      "id": "3",
-      "name": "NewYork",
-      "country": "USA",
-      "type": "city"
-    },
-    {
-      "id": "4",
-      "name": "Parma",
-      "country": "Italy",
-      "type": "city"
-    },
-    {
-      "id": "5",
-      "name": "Naples",
-      "country": "Italy",
-      "type": "city"
-    },
+const leftParam = [
+  {
+    type: 'city',
+    hasLeft: ['openingParentheses', 'operator'],
+    props: [
+      {
+        "id": "1",
+        "name": "Phoenix",
+        "country": "USA"
+      },
+      {
+        "id": "2",
+        "name": "Florida",
+        "country": "USA"
+      },
+      {
+        "id": "3",
+        "name": "NewYork",
+        "country": "USA"
+      },
+      {
+        "id": "4",
+        "name": "Parma",
+        "country": "Italy"
+      },
+      {
+        "id": "5",
+        "name": "Naples",
+        "country": "Italy"
+      },
 
-    {
-      "id": "6",
-      "name": "Paris",
-      "country": "France",
-      "type": "city"
-    },
-    {
-      "id": "7",
-      "name": "Nice",
-      "country": "France",
-      "type": "city"
-    },
-  ]
-}
+      {
+        "id": "6",
+        "name": "Paris",
+        "country": "France"
+      },
+      {
+        "id": "7",
+        "name": "Nice",
+        "country": "France"
+      },
+    ]
+  },
+]
